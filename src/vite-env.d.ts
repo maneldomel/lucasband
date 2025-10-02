8e4e8dd0e344be34f59d4831815602339c6a66ab
maneldomel <emanuelmilk25@gmail.com>
2025-10-02 20:43:45 -0300

    Updated index.html

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    'vturb-smartplayer': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+      id?: string;
+    };
+  }
+}
