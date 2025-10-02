49546634752f4310e8b69a91b3228f1cd8e67708
maneldomel <emanuelmilk25@gmail.com>
2025-10-02 20:57:09 -0300

    Updated image copy.png

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,1 @@
 /// <reference types="vite/client" />
-
-declare namespace JSX {
-  interface IntrinsicElements {
-    'vturb-smartplayer': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-      id?: string;
-    };
-  }
-}
